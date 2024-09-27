e4372229b5e8a8c137479516d6e2cee7353a5512
Douglas Dachicourt <8815@holbertonstudents.com>
2024-09-27 11:29:55 +0200

    try

--- TypeScript/task1/js/main.ts
+++ TypeScript/task1/js/main.ts
@@ -38,6 +38,31 @@ const printTeacher: printTeacherFunction = (firstName, lastName) => {
 
 // TASK 4
 
-class StudentClass {
-    
+interface StudentConstructor {
+    new (firstName: string, lastName: string): StudentInterface;
+}
+
+interface StudentInterface {
+    firstName: string;
+    lastName: string;
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+class StudentClass implements StudentInterface {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
 }
